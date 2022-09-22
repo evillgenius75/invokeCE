37806a6dba412185bff6f7796aed42cea9bdd27c
Eddie Villalba <eddiejv@google.com>
2022-09-22 18:24:31 +0000

    move user input to a function

--- main.go
+++ main.go
@@ -11,12 +11,7 @@ import (
 const EXPERT = "Customer Engineer"
 
 func main() {
-	var salesStage int32
-	fmt.Print("Enter your sales stage: ")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	stage, _ := strconv.Atoi(strings.TrimSuffix(input, "\n"))
-	salesStage = int32(stage)
+	salesStage := getSalesStage()
 	if EXPERT == "Customer Engineer" && salesStage < 4 && salesStage > 1 {
 		invokeCE(salesStage)
 	}
@@ -25,8 +20,16 @@ func main() {
 func invokeCE(stage int32) {
 	switch stage {
 	case 1, 2, 3, 4:
-		fmt.Println("go/new-er")
+		fmt.Printf("A %s will help you there. go/new-er\n", EXPERT)
 	default:
 		fmt.Println("Call an FSR")
 	}
+}
+
+func getSalesStage() int32 {
+	fmt.Print("Enter your sales stage: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	stage, _ := strconv.Atoi(strings.TrimSuffix(input, "\n"))
+	return int32(stage)
 }
