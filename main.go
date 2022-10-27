5856b812992814913bcd86e91ef721d927bc87b9
Eddie Villalba <eddiejv@google.com>
2022-10-27 20:28:30 +0000

    changed for a cli version

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"errors"
+	"flag"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 const expert = "Customer Engineer"
 
+var salesStage int
+
 func main() {
-	salesStage := getSalesStage()
+	flag.IntVar(&salesStage, "sales-stage", 0, "Current Sales stage you are in (0-4)")
+	flag.Parse()
 	msg, err := invokeCE(salesStage)
 	if err != nil {
 		fmt.Printf("ERROR: %s, engage a %s and let's get this done!", err, expert)
@@ -20,21 +20,13 @@ func main() {
 	fmt.Println(msg)
 }
 
-func invokeCE(stage int32) (string, error) {
-	switch {
-	case stage > 0 && stage <= 4:
+func invokeCE(stage int) (string, error) {
+	switch stage {
+	case 0:
+		return "", errors.New("need help qualifying")
+	case 1, 2, 3, 4:
 		return fmt.Sprintf("A %s will help you with that. go/new-er", expert), nil
-	case stage < 0 || stage >= 5:
-		return "", fmt.Errorf("%d is not a valid Sales Stage", stage)
 	default:
-		return "", errors.New("need help qulaifying")
+		return "", fmt.Errorf("%d is not a valid Sales Stage", stage)
 	}
-}
-
-func getSalesStage() int32 {
-	fmt.Print("Enter your sales stage (1-4), or any character if unknown: ")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	stage, _ := strconv.Atoi(strings.TrimSuffix(input, "\n"))
-	return int32(stage)
 }
